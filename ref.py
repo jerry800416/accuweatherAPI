8a3d0510729430e1165cbc0ca6ba03725fa81775
jerry800416 <s123962474@gmail.com>
2019-10-23 16:08:39 +0800

    屏蔽敏感資訊

--- ref.py
+++ ref.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # db_connect
-db_info = ('211.23.162.130','gridwell','gridwell123',3306)
-# db_info = ('localhost','gridwell','gridwell123',3306)
-db_info2 = ('140.112.94.59','gridwell','gridwell123',30303)
+db_info = ()
+# db_info = ()
+db_info2 = ()
 select_db_name = "Gridwell"
 insert_db_name = "DTR_realtime_acc"
 pre_db_name = "DTR_forcast_acc"
