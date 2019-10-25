9354cba95ef79fecbaf654b4c0f265fbc049ccd6
jerry800416 <s123962474@gmail.com>
2019-10-25 17:19:12 +0800

    修改拉取資料時間由整點拉取改為前1小時55分鐘拉取

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 The code for grabbing weather data from AccuWeather<br>
 <br>
 
+### 2019.10.25 修改
+1.修改拉取資料時間由整點拉取改為前1小時55分鐘拉取<br>
+
 ### 2019.10.23 新增
 1.新增塔基相關電塔<br>
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from datetime import datetime,timedelta
 from lib import connect_DB,get_api_key,acc_weather_api,return_key_status,go_to_log
 import ref
 
@@ -11,22 +11,22 @@ import ref
 # 補齊註解
 #######################################
 
-now = datetime.now()
+now = datetime.now()+timedelta(hours=1)
 # 獲取有哪些塔需要拉取天氣資訊
 segids = connect_DB(ref.db_info,'Gridwell',ref.sql,'select',0)
 segids_tb = connect_DB(ref.db_info,'TowerBase_Gridwell',ref.sql,'select',0)
 # 獲取目前可用的api_key
-api_key = get_api_key(ref.db_info,ref.select_db_name)
+api_key = get_api_key(ref.db_info,'Gridwell')
 
 # 拉取天氣資訊並push 上DB(架空)
 for segid in segids:
     api_key = acc_weather_api(now,segid[0],api_key,ref.log_path,'Gridwell')
 
-# 拉取天氣資訊並push 上DB(風速)
+# 拉取天氣資訊並push 上DB(塔基)
 for segid_tb in segids_tb:
     api_key = acc_weather_api(now,segid_tb[0],api_key,ref.log_path,'TowerBase_Gridwell')
 
 # 每天？點的時候重置api_key的status
-return_key_status(ref.db_info,ref.select_db_name,4)
+return_key_status(ref.db_info,'Gridwell',4)
 
 go_to_log(ref.log_path,'all data complete')
